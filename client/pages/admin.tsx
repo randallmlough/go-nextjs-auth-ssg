128873450e88ce20953e3a74d3d02a11b10b3f19
Randy <randallmlough@gmail.com>
2021-12-31 13:59:46 -0700

    admin text

--- client/pages/admin.tsx
+++ client/pages/admin.tsx
@@ -18,11 +18,11 @@ export default function Profile() {
           <div className="px-4 sm:p-6">
             <div>
               <h3 className="text-lg leading-6 font-medium text-gray-900">
-                Restricted
+                Admin
               </h3>
               <p className="mt-1 text-sm text-gray-500">
-                This is a demo project meant to show off different technologies.
-                <strong> Do not give sensitive information</strong>.
+                This is a protected admin page. Only the demo account should
+                have access to this page.
               </p>
             </div>
           </div>
